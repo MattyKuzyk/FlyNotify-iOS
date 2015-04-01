3e40de3d900109d42f44f41ed54bcc95a6f6d83f
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2015-04-01 08:51:49 -0700

    Initial Commit

---
+++ FlyNotifyTests/FlyNotifyTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlyNotifyTests.swift
+//  FlyNotifyTests
+//
+//  Created by Matthew Kuzyk on 4/1/15.
+//  Copyright (c) 2015 Matthew Kuzyk. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FlyNotifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
